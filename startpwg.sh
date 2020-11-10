3627a5b2aadb1824c42119e3a2ca8c5337c0139c
dcrntn <david.szegfu97@gmail.com>
2020-11-10 12:22:25 +0100

    Added auto download if not available on PLC

--- startpwg.sh
+++ startpwg.sh
@@ -1,2 +1,14 @@
 #!/bin/bash  
-~/./pwg
+
+FILE="/etc/wsgateway/pwg"
+
+if [[ -f "$FILE" ]]; then
+    /etc/wsgateway/./pwg
+else
+    wget https://github.com/dcrntn/pwg/blob/main/pwg?raw=true
+    mkdir /etc/wsgateway
+    cp pwg?raw=true /etc/wsgateway/pwg
+    rm pwg?raw=true
+    chmod +x /etc/wsgateway/pwg
+    /etc/wsgateway/./pwg
+fi

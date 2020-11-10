f10b0c84365219dab7dae27482236ba1f098a665
dcrntn <david.szegfu97@gmail.com>
2020-11-10 12:41:40 +0100

    Added dir check

--- startpwg.sh
+++ startpwg.sh
@@ -1,12 +1,15 @@
 #!/bin/bash  
 
 FILE="/etc/wsgateway/pwg"
+DIR="/etc/wsgateway"
 
 if [[ -f "$FILE" ]]; then
     /etc/wsgateway/./pwg
 else
     wget https://github.com/dcrntn/pwg/blob/main/pwg?raw=true
-    mkdir /etc/wsgateway
+    if [[ ! -d "$DIR"]] then
+        mkdir /etc/wsgateway
+    fi
     cp pwg?raw=true /etc/wsgateway/pwg
     rm pwg?raw=true
     chmod +x /etc/wsgateway/pwg
